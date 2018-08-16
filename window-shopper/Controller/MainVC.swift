45b7305f4b3ca21cd897f51629c85c70f32a608a
Asherlee Levy <asherleelevy@gmail.com>
2018-08-16 18:11:17 -0400

    Created a custom func and button to calculate the numbers inputted

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calBtn.backgroundColor = #colorLiteral(red: 0.9746767241, green: 0.5594085431, blue: 0.003124905569, alpha: 1)
+        calBtn.setTitle("calculate", for: .normal)
+        calBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calBtn
+        priceTxt.inputAccessoryView = calBtn
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        print("hey we got here")
+        
     }
-
 
 }
 
